2bf78e9973f954f0bee38d0b445af5bd2206bef4
abisha080901 <abisha.s@lluminantworks.com>
2025-08-01 12:58:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_List:
       
 import requests
 smoothiefroot_response = requests.get(" https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-#sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
